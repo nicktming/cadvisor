bb62a91de506cebce8a161c071bb2acd8cb96b00
nicktming <2279563278@qq.com>
2020-09-08 18:21:28 +0800

    add container

---
+++ cmd/internal/container/install/install.go
@@ -0,0 +1,10 @@
+package install
+
+
+import (
+	//_ "github.com/google/cadvisor/cmd/internal/container/mesos/install"
+	//_ "github.com/google/cadvisor/container/containerd/install"
+	//_ "github.com/google/cadvisor/container/crio/install"
+	_ "github.com/google/cadvisor/container/docker/install"
+	//_ "github.com/google/cadvisor/container/systemd/install"
+)
